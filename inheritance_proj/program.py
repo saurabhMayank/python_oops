28850e96e00a3b2de94417913acfa202f6bf56a0
mayank_saurabh <mayank.saurabh@elucidata.io>
2021-06-04 20:57:35 +0530

    [WIP]:Small project relating to Inheritance modeling the HR system in a company

---
+++ inheritance_proj/program.py
@@ -0,0 +1,13 @@
+import hr
+
+salary_employee = hr.SalaryEmployee(1, 'John Smith', 1500)
+hourly_employee = hr.HourlyEmployee(1, 'John Doe', 40, 15)
+commission_employee = hr.CommisionEmployee(3, 'kevin becon', 1000, 250)
+
+# Here if we pass generic_employee obj into the calculate payroll_method
+# of payroll system then it will break as generic_employee does not have calculate_payroll func
+# method... Employee Class should be a abstract class which should only be inherited.. Not Instantiated
+#generic_employee = hr.Employee(4," Jaden Smith")
+
+payroll_system = hr.PayrollSystem()
+payroll_system.calculate_payroll([salary_employee, hourly_employee, commission_employee])
