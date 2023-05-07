e52cbbd00f14907ba99039f9d3f88abd4b845f7e
saurabhMayank <82028762+saurabhMayank@users.noreply.github.com>
2023-05-07 11:27:14 +0530

    Update encapsulation_java_example.py

--- encapsulation_java_example.py
+++ encapsulation_java_example.py
@@ -4,6 +4,23 @@ package com.ramesh.ood.concepts.encapsulation;
 //--- STEP 00 - WHAT IS ABSTRACTION & ENCAPSULATION? 
 //---------------------------------------
 
+/**
+Abstraction in layman terms (from chatgpt)
+Abstraction is a concept in software engineering that involves reducing complexity by hiding unnecessary details and focusing on essential features. 
+It allows us to create simplified models of complex systems, which can be easier to understand, use, and maintain.
+Think of abstraction like a TV remote control. When you use a remote control, you don't need to know how the TV works or how the signal 
+is transmitted to change the channel. You simply press a button and the TV responds. The remote control abstracts away the complex details of the 
+TV system, allowing you to interact with it in a simple and intuitive way.
+Similarly, in software engineering, we use abstraction to hide complex implementation details and focus on essential features. 
+For example, we might create a high-level interface for a database system that allows us to perform simple operations like adding, deleting, 
+or querying records, without needing to know the details of how the data is stored or managed.
+Abstraction also allows us to create modular, reusable components that can be used in different contexts. By separating out the essential 
+features of a system and hiding the implementation details, we can create components that are more flexible, adaptable, and maintainable.
+Overall, abstraction is an important concept in software engineering that helps us to manage complexity and create systems that are easier 
+to understand, use, and maintain.
+
+**/
+
 ///** 
 // * Abstraction = Looking only at the information that is relevant at the time. 
 // *
@@ -21,6 +38,23 @@ package com.ramesh.ood.concepts.encapsulation;
 // * accessed by other code defined outside the wrapper. 
 // * For example - if a field is declared private, it cannot be accessed by anyone outside the class, thereby hiding the fields within the class. 
 // */
+
+/**
+In Layman terms Encapsulation (From Chat GPT)
+Encapsulation is a concept in object-oriented programming that involves grouping together related data and functions into a single unit, called a class. 
+This unit acts as a protective wrapper around the data and functions, preventing other parts of the program from accessing or modifying them directly.
+Think of encapsulation like a gift box. The gift inside the box represents the data and functions in a class. 
+The box itself represents the encapsulation,  or protection, around the gift.
+By encapsulating the data and functions in a class, we can control how they are accessed and used. We can define specific methods, or functions, 
+that can be used to access or modify the data, while keeping the data itself hidden from other parts of the program.
+This has several benefits, including improved security, more efficient code, and easier maintenance.
+Encapsulation also helps to prevent unintended side effects that can occur when data is accessed or modified directly.
+Overall, encapsulation is an important concept in object-oriented programming that helps to improve the organization, efficiency, 
+and security of software.
+**/
+
+
+
 
 //---------------------------------------
 //--- STEP 01 - UNDERSTAND ENCAPSULATION BY EXAMPLE
