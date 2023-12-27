acd2f149451838518d458dfdba6f6b44a72da806
Mayank <82028762+saurabhMayank@users.noreply.github.com>
2023-12-27 16:14:41 +0530

    Update encapsulation_java_example.py

--- encapsulation_java_example.py
+++ encapsulation_java_example.py
@@ -4,31 +4,7 @@ package com.ramesh.ood.concepts.encapsulation;
 //--- STEP 00 - WHAT IS ABSTRACTION & ENCAPSULATION? 
 //---------------------------------------
 
-/**
-Abstraction in layman terms (from chatgpt)
-Abstraction is a concept in software engineering that involves reducing complexity by hiding unnecessary details and focusing on essential features. 
-It allows us to create simplified models of complex systems, which can be easier to understand, use, and maintain.
-Think of abstraction like a TV remote control. When you use a remote control, you don't need to know how the TV works or how the signal 
-is transmitted to change the channel. You simply press a button and the TV responds. The remote control abstracts away the complex details of the 
-TV system, allowing you to interact with it in a simple and intuitive way.
-Similarly, in software engineering, we use abstraction to hide complex implementation details and focus on essential features. 
-For example, we might create a high-level interface for a database system that allows us to perform simple operations like adding, deleting, 
-or querying records, without needing to know the details of how the data is stored or managed.
-Abstraction also allows us to create modular, reusable components that can be used in different contexts. By separating out the essential 
-features of a system and hiding the implementation details, we can create components that are more flexible, adaptable, and maintainable.
-Overall, abstraction is an important concept in software engineering that helps us to manage complexity and create systems that are easier 
-to understand, use, and maintain.
 
-**/
-
-///** 
-// * Abstraction = Looking only at the information that is relevant at the time. 
-// *
-// * Abstraction is the process or result of generalization by reducing the information content of a concept or an observable phenomenon, 
-// * typically in order to retain only information which is relevant for a particular purpose. It is starting point of design.
-// * Functional abstraction - means that a function can be used without taking into account how the function is implemented. 
-// * Data Abstraction - means that data can be used without taking into account how the data are stored.
-// */
 
 ///**
 // * Encapsulation = Data hiding mechanism.
